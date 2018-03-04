6b5226eb4de90868d5ecb7c52d02437aee9ddea8
Kenan Deng <kenan.deng@mail.utoronto.ca>
2018-03-04 09:02:18 -0500

    Add some comments

---
+++ Rational.h
@@ -0,0 +1,20 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+namespace igl
+{
+	namespace bol{
+		class Rational{
+			int numerator;
+			int denominator;
+			//Need a constructor construct this from float and double
+			//See https://stackoverflow.com/questions/16297520/how-to-convert-ieee754-doubles-into-fraction-of-two-integers
+			//Rational(float );
+			
+			//Also need to support all basic arithmetics: + - * / , ++, --, %, >, <, ==,
+		}
+	
+
+	}
+}
+#endif
---
+++ mesh_boolean.h
@@ -0,0 +1,11 @@
+#ifndef MESH_BOOLEAN_H
+#define MESH_BOOLEAN_H
+namespace igl
+{
+	namespace bol
+	{
+		//The main function to work on
+		void mesh_boolean();
+	}
+}
+#endif
