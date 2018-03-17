a1bac2427eb89f2cc32793a4a8ad070d73663c31
Kenan <dknyxh@gmail.com>
2018-03-17 17:55:30 -0400

    Add boost include

--- mesh_boolean.cpp
+++ mesh_boolean.cpp
@@ -1,5 +1,5 @@
 #include "mesh_boolean.h"
-
+#include "remesh_self_intersections.h"
 
 
 
@@ -19,11 +19,29 @@ bool mesh_boolean(
   Eigen::PlainObjectBase<DerivedVC > & VC,
   Eigen::PlainObjectBase<DerivedFC > & FC)
 {
+  using namespace igl::bol;
   //First combine VA FA, VB, FB into VV, FF
+  Eigen::Matrix<typename DerivedVA::Scalar,Eigen::Dynamic,3> VV(VA.rows() + VB.rows(), 3);
+  DerivedFC FF(FA.rows() + FB.rows(), 3);
+  // Can't use comma initializer
+  for(int a = 0;a<VA.rows();a++)
+  {
+    for(int d = 0;d<3;d++) VV(a,d) = VA(a,d);
+  }
+  for(int b = 0;b<VB.rows();b++)
+  {
+    for(int d = 0;d<3;d++) VV(VA.rows()+b,d) = VB(b,d);
+  }
+  FF.block(0, 0, FA.rows(), 3) = FA;
+  // Eigen struggles to assign nothing to nothing and will assert if FB is empty
+  if(FB.rows() > 0)
+  {
+    FF.block(FA.rows(), 0, FB.rows(), 3) = FB.array() + VA.rows();
+  }
   
-  
-  //stage 1: remesh_self intersection
-  
+  //stage 1: remesh_self intersection on VV FF
+  //call remesh self intersection
+  //remesh_self_intersection
   
   //stage 2: extract cell
   
