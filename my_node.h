cea3106078bfdf4e67deebbdd8f3ce5170229af3
Ziheng Liang <ziheng941008@hotmail.com>
2018-04-02 02:49:08 -0400

    delaunay_traiangulation is now tested with 4 points

--- my_node.h
+++ my_node.h
@@ -3,6 +3,7 @@
 
 #include "polygon.h"
 #include <Eigen/Dense>
+#include <iostream>
 
 namespace igl
 {
@@ -19,7 +20,10 @@ namespace igl
         };
 
         inline void remove_edge_from_node(Node* node, int i, int j) {
+        	std::cout << "remove_edge_from_node1" << std::endl;
         	for (int k = 0; k < node->size; k++) {
+        		std::cout << k << std::endl;
+        		std::cout << node->edges.size() << std::endl;
         		if ((get<0>(node->edges.at(k)) == i && get<1>(node->edges.at(k)) == j) || 
         			(get<1>(node->edges.at(k)) == i && get<0>(node->edges.at(k)) == i)) {
         			node->edges.erase(node->edges.begin()+k);
@@ -27,6 +31,7 @@ namespace igl
         			break;
         		}
         	}
+        	std::cout << "remove_edge_from_node2" << std::endl;
         	for (int k = 0; k < node->polygons.size(); k++) {
         		if (exist_edges(node->polygons.at(k), i, j) != -1) {
         			node->polygons.erase(node->polygons.begin()+k);
