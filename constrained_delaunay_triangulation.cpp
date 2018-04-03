5ebfb4551854e13b8e4c4056df5c9245f63dd91b
Ziheng Liang <ziheng941008@hotmail.com>
2018-04-03 16:44:06 -0400

    cleanup debug code

--- constrained_delaunay_triangulation.cpp
+++ constrained_delaunay_triangulation.cpp
@@ -12,14 +12,9 @@ using namespace std;
 void igl::bol::constrained_delaunay_triangulation(const Eigen::MatrixXd &V, const Eigen::MatrixXi &C, Eigen::MatrixXi & F) {
 	Node* root = new Node();
 	MatrixXd projectV;
-	cout << "test1" << endl;
 	contruct_tree(V, root, projectV);
-	cout << "test2" << endl;
 	delaunay_triangulation(projectV, root);
-	cout << "test3" << endl;
 	add_constrained(projectV, C, root);
-	cout << "test4" << endl;
-	cout << root->polygons.size() << endl;
 	F = Eigen::MatrixXi::Zero(root->polygons.size(), 3);
 	for (int i = 0; i < F.rows(); i++) {
 		F(i, 0) = root->polygons.at(i)->vertex(0);
@@ -30,22 +25,16 @@ void igl::bol::constrained_delaunay_triangulation(const Eigen::MatrixXd &V, cons
 
 void igl::bol::contruct_tree(const MatrixXd &V, Node* node, MatrixXd &projectV) {
 	assert (V.rows() >= 3);
-	cout << "test1.0" << endl;
 	RowVector3d p1 = V.row(0);
 	RowVector3d p2 = V.row(1);
 	RowVector3d p3 = V.row(2);
-	cout << "test1.1" << endl;
 	RowVector3d up = (p2-p1).cross(p3-p1);
 	RowVector3d x = (p2-p1).normalized();
 	RowVector3d y = up.cross(x).normalized();
-	cout << x << endl;
-	cout << y << endl;
-	cout << "test1.2" << endl;
 	MatrixXd Vn = V.rowwise() - p1;
 	MatrixXd A = MatrixXd::Zero(2, 3);
 	A << x(0), x(1), x(2), y(0), y(1), y(2);
 	MatrixXd piA = A.transpose()*(A * A.transpose()).inverse();
-	cout << "test1.3" << endl;
 	projectV = V * piA;
 	MatrixXd temp;
 	MatrixXi xindex;
@@ -54,11 +43,8 @@ void igl::bol::contruct_tree(const MatrixXd &V, Node* node, MatrixXd &projectV) 
 	projectV.col(0) = V.col(0);
 	projectV.col(1) = V.col(1);
 
-	cout << "test1.4" << endl;
 	igl::sort(projectV, 1, true, temp, xindex);
-	cout << "test1.5" << endl;
 	subdivide(xindex.col(0), node);
-	cout << "test1.6" << endl;
 }
 
 void igl::bol::subdivide(Eigen::VectorXi index, Node* node) {
@@ -87,23 +73,22 @@ void igl::bol::break_polygons(Eigen::MatrixXd V, Eigen::RowVectorXi C, std::vect
 	int vidx1 = -1, vidx2 = -1;
 	Polygon* start, *next;
 	bool intersection = false;
-	cout << "test3.1" << endl;
 	for (int i = 0; i < polygons.size(); i++) {
 		vidx1 = find_vertex(polygons.at(i), C(0));
-		cout << vidx1 << endl;
+
 		if (vidx1 != -1) {
 			for (int j = 1; j < polygons.at(i)->size; j++) {
-				cout << "test3.11" << endl;	
-				cout << C(0) << endl;
-				cout << C(1) << endl;
-				cout << polygons.at(i)->vertex(j-1) << endl;
-				cout << polygons.at(i)->vertex(j) << endl;
+		
+		
+		
+		
+		
 				if (intersect(V.row(C(0)), V.row(C(1)), 
 							  V.row(polygons.at(i)->vertex(j-1)), 
 							  V.row(polygons.at(i)->vertex(j)))) {
 					intersection = true;
 					next = polygons.at(i)->adjacent_polygon.at(j);
-					cout << "found" << endl;
+			
 					break;
 				}
 			}
@@ -120,7 +105,7 @@ void igl::bol::break_polygons(Eigen::MatrixXd V, Eigen::RowVectorXi C, std::vect
 							  V.row(polygons.at(i)->vertex(j)))) {
 					intersection = true;
 					next = polygons.at(i)->adjacent_polygon.at(j);
-					cout << "found" << endl;
+			
 					break;
 				}
 			}
@@ -131,22 +116,21 @@ void igl::bol::break_polygons(Eigen::MatrixXd V, Eigen::RowVectorXi C, std::vect
 		}
 	}
 	if (start) {
-		cout << "has_start" << endl;
+
 	}
 	if (next) {
-		cout << "has_next" << endl;
+
 	}
 	if (!start && !next) {
 		return;
 	}
 
-	cout << "test3.2" << endl;
 	polygons.erase(std::remove(polygons.begin(), polygons.end(), start), polygons.end());
 	Polygon* new_polygon = new Polygon();
 	while (vidx1 == -1 || vidx2 == -1) {
-		cout << "test3.26" << endl;
+
 		merge(start, next, new_polygon);
-		cout << "test3.25" << endl;
+
 		polygons.erase(std::remove(polygons.begin(), polygons.end(), next), polygons.end());
 		for (int j = 0; j < new_polygon->size - 1; j++) {
 			if (intersect(V.row(C(0)), V.row(C(1)), 
@@ -168,16 +152,12 @@ void igl::bol::break_polygons(Eigen::MatrixXd V, Eigen::RowVectorXi C, std::vect
 	MatrixXi xindex;
 	MatrixXd sub_V;
 	VectorXi cols;
-	cout << "test3.3" << endl;
 	igl::colon(0,V.cols()-1, cols);
 	igl::slice(V,new_polygon->vertex,cols,sub_V);
 	igl::slice(index,new_polygon->vertex,cols,xindex);
-	cout << "test3.4" << endl;
 	Node* node = new Node();
 	subdivide(xindex, node);
-	cout << "test3.5" << endl;
 	delaunay_triangulation(sub_V, node);
-	cout << "test3.6" << endl;
 	polygons.insert(polygons.end(), node->polygons.begin(), node->polygons.end());
 }
 
@@ -190,8 +170,8 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 		assert(node->left);
 	}
 	if (!node->left && !node->right) {
-		cout << "node->size" << endl;
-		cout << node->size << endl;
+
+
 		if (node->size == 3) {
 			Polygon* polygon = new Polygon();
 			polygon->vertex = VectorXi::Zero(node->size);
@@ -211,13 +191,13 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 		}
 	}
 	else {
-		cout << "test2.1" << endl;
-		cout << "node->right->edges.size()" << endl;
-		cout << node->right->edges.size() << endl;
+
+
+
 		delaunay_triangulation(V, node->left);
 		delaunay_triangulation(V, node->right);
-		cout << "node->right->edges.size()" << endl;
-		cout << node->right->edges.size() << endl;
+
+
 
 		// get partial Vs
 		MatrixXd left_sub_V;
@@ -226,8 +206,8 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 		igl::colon(0,V.cols()-1, cols);
 		igl::slice(V,node->left->index,cols,left_sub_V);
 		igl::slice(V,node->right->index,cols,right_sub_V);
-		cout << "test2.2" << endl;
-		cout << left_sub_V << endl;
+
+
 
 		// get vertical orders of each sub tree
 		MatrixXi left_vert_index;
@@ -235,7 +215,7 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 		MatrixXd temp;
 		igl::sort(left_sub_V, 1, true, temp, left_vert_index);
 		igl::sort(right_sub_V, 1, true, temp, right_vert_index);
-		cout << "test2.25" << endl;
+
 
 		// find the base LR-edge
 		int l_vidx = 0, r_vidx = 0;
@@ -243,19 +223,19 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 			RowVectorXd p1 = left_sub_V.row(left_vert_index(l_vidx,1));
 			RowVectorXd p2 = right_sub_V.row(right_vert_index(r_vidx,1));
 			bool has_intersection = false;
-			cout << "test2.27" << endl;
+	
 			for (int i = 0; i < node->left->index.rows(); i++) {
 				// need to skip self
 				RowVectorXd p3 = V.row(get<0>(node->left->edges.at(i)));
 				RowVectorXd p4 = V.row(get<1>(node->left->edges.at(i)));
-				cout << "test2.275" << endl;
+		
 				if (intersect(p1, p2, p3, p4)) {
 					l_vidx ++;
 					has_intersection = true;
 					break;
 				}
 			}
-			cout << "test2.28" << endl;
+	
 			for (int i = 0; i < node->right->index.rows(); i++) {
 				RowVectorXd p3 = V.row(get<0>(node->right->edges.at(i)));
 				RowVectorXd p4 = V.row(get<1>(node->right->edges.at(i)));
@@ -269,24 +249,24 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 				break;
 			}
 		}
-		cout << "test2.3" << endl;
-		cout << node->index << endl;
-		cout << node->left->index << endl;
-		cout << node->right->index << endl;
+
+
+
+
 
 		vector<tuple<int,int>> new_edges;
 		new_edges.push_back(make_tuple(node->left->index(l_vidx), node->right->index(r_vidx)));
 
 		int l_ridx = node->left->index(l_vidx);
 		int r_ridx = node->right->index(r_vidx);
-		cout << "test2.4" << endl;
-		cout << node->left->index(l_vidx) << endl;
-		cout << node->right->index(r_vidx) << endl;
+
+
+
 		// if no more edges are found, triangulation is complete
 		int l_candidate_final;
 		int r_candidate_final;
 		do {
-			cout << "loooooooooop" << endl;
+	
 			// find candidates from both side
 			vector<tuple<int,int>> r_candidate;
 			vector<tuple<int,int>> l_candidate;
@@ -305,19 +285,19 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 				}
 				// to do: need to check angle clock vs counter-clock
 				double a = angle(V.row(r_ridx), V.row(l_ridx), V.row(other));
-				cout << "angle: " << endl;
-				cout << r_ridx << endl;
-				cout << other << endl;
-				cout << l_ridx << endl;
-				cout << a << endl;
+		
+		
+		
+		
+		
 				if (a > 3.1415926535) {
 					break;
 				}
 				if (a < 0) {
 					break;
 				}
-				cout << "angle:" << endl;
-				cout << a << endl;
+		
+		
 				// add it to a specific location based on angle
 				bool added = false;
 				for (int j = 0; j < r_candidate.size(); j++) {
@@ -326,18 +306,18 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 					}
 					else if (a < get<1>(r_candidate.at(j))){
 						r_candidate.insert(r_candidate.begin() + j, make_tuple(other, a));
-						cout << "added to r_candidate1" << endl;
+				
 						added = true;
 					}
 					// add it to the end if not added before
 				}
 				if (!added) {
-					cout << "added to r_candidate2" << endl;
+			
 					r_candidate.push_back(make_tuple(other, a));
 				}
 			}
 
-			cout << "test2.5" << endl;
+	
 
 			for (int i = 0; i < node->left->edges.size(); i++) {
 				int other;
@@ -354,11 +334,11 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 				}
 				// to do: need to check angle clock vs counter-clock
 				double a = angle(V.row(l_ridx), V.row(other), V.row(r_ridx));
-				cout << "angle: " << endl;
-				cout << r_ridx << endl;
-				cout << other << endl;
-				cout << l_ridx << endl;
-				cout << a << endl;
+		
+		
+		
+		
+		
 				if (a > 3.1415926535) {
 					break;
 				}
@@ -381,15 +361,15 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 				}
 			}
 			// find final candidate from both side
-			cout << "test2.51" << endl;
+	
 			RowVectorXd p_left = V.row(l_ridx);
 			RowVectorXd p_right = V.row(r_ridx);
 			RowVectorXd center;
-			cout << r_candidate.size() << endl;
+	
 			l_candidate_final = -1;
 			r_candidate_final = -1;
 			for (int i = 0; i < r_candidate.size(); i++) {
-				cout << "test2.515" << endl;
+		
 				if ((i + 1) == r_candidate.size()) {
 					r_candidate_final = get<0>(r_candidate.at(i));
 					break;
@@ -403,19 +383,19 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 				}
 				// if not, remove this edge
 				else {
-					cout << "test2.52" << endl;
+			
 					remove_edge_from_node(node->right, r_ridx, get<0>(r_candidate.at(i)));
-					cout << "test2.53" << endl;
+			
 				}
 			}
 
-			cout << "test2.6" << endl;
-			cout << l_candidate.size() << endl;
+	
+	
 			if (l_candidate.size() == 1) {
 				l_candidate_final = get<0>(l_candidate.at(0));
 			}
 			for (int i = 0; i < l_candidate.size(); i++) {
-				cout << "test2.61" << endl;
+		
 				if ((i + 1) == l_candidate.size()) {
 					l_candidate_final = get<0>(l_candidate.at(i));
 					break;
@@ -423,21 +403,21 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 				get_circle_center(p_left, p_right, V.row(get<0>(l_candidate.at(i))), center);
 				if ((p_left-center).dot(p_left-center) < (V.row(get<0>(l_candidate.at(i+1))) - center)
 					.dot(V.row(get<0>(l_candidate.at(i+1))))) {
-					cout << "test2.611" << endl;
+			
 					l_candidate_final = get<0>(l_candidate.at(i));
 					break;
 				}
 				else {
-					cout << "test2.62" << endl;
+			
 					remove_edge_from_node(node->left, l_ridx, get<0>(l_candidate.at(i)));
-					cout << "test2.63" << endl;
+			
 				}
 			}
 
 			// final step
-			cout << "test2.7" << endl;
-			cout << l_candidate_final << endl;
-			cout << r_candidate_final << endl;
+	
+	
+	
 			if (l_candidate_final == -1 && r_candidate_final != -1) {
 				r_ridx = r_candidate_final;
 			}
@@ -459,15 +439,15 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 			}
 			Polygon* new_polygon =  new Polygon();
 
-			cout << "test2.75" << endl;
+	
 			new_polygon->size = 3;
-			cout << "test2.76" << endl;
+	
 			new_polygon->vertex = VectorXi::Zero(3);
 			new_polygon->vertex(0) = r_ridx;
-			cout << "test2.77" << endl;
-			cout << "test2.78" << endl;
+	
+	
 			new_polygon->vertex(1) = l_ridx;
-			cout << "test2.8" << endl;
+	
 			// up is always NULL, will be updated in the future
 			new_polygon->adjacent_polygon.push_back(NULL);
 			new_polygon->adjacent_index.push_back(NULL);
@@ -497,7 +477,7 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 				}
 			}
 			else {
-				cout << "test2.92" << endl;
+		
 				new_polygon->vertex(2) = get<0>(new_edges.at(new_edges.size()-1));
 				for (int i = 0; i < node->right->polygons.size();i++) {
 					int oppo_idx = exist_edges(node->left->polygons.at(i), 
@@ -530,10 +510,10 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 
 			new_edges.push_back(make_tuple(l_ridx, r_ridx));
 			node->polygons.push_back(new_polygon);
-			cout << new_edges.size() << endl;
-			cout << l_ridx << endl;
-			cout << r_ridx << endl;
-			cout << "end loooooooooop" << endl;
+	
+	
+	
+	
 		} while ((l_candidate_final != -1) || (r_candidate_final != -1));
 		// add all new edges into the list
 		node->edges.insert(node->edges.end(), node->left->edges.begin(), node->left->edges.end());
@@ -548,11 +528,11 @@ void igl::bol::delaunay_triangulation(MatrixXd V, Node* node) {
 
 // need to determine clockwise angle or counter-clockwise angle
 // double igl::bol::angle(RowVectorXd p1, RowVectorXd p2, RowVectorXd p3) {
-// 	// cout << "inside angle" << endl;
-// 	// cout << p1 << endl;
-// 	// cout << p2 << endl;
-// 	// cout << p3 << endl;
-// 	// cout << (p2 - p1).dot(p3 - p1) / ((p2 - p1).norm() * (p3 - p1).norm()) << endl;
+// 	/
+// 	/
+// 	/
+// 	/
+// 	/
 // 	return acos((p2 - p1).dot(p3 - p1) / ((p2 - p1).norm() * (p3 - p1).norm()));
 
 // }
