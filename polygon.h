826fc456d09c45fd19c3fd9c1271aef44fa25a18
Ziheng Liang <ziheng941008@hotmail.com>
2018-04-04 13:24:55 -0400

    cdt finished

--- polygon.h
+++ polygon.h
@@ -17,14 +17,14 @@ namespace igl
             std::vector<int> adjacent_index; 
         };
 
-        inline void split (Polygon* ab, Polygon* a, Polygon* b, std::tuple<int,int> new_edge) {
+        inline void split (Polygon* ab, Polygon* a, Polygon* b, int i1, int i2) {
             int idx1=-1, idx2=-1;
         	for (int i = 0; i < ab->size; i++) {
                 int current = ab->vertex(i);
-                if (current == std::get<0>(new_edge)) {
+                if (current == i1) {
                     idx1 = i;
                 }
-                else if (current == std::get<1>(new_edge)) {
+                else if (current == i2) {
                     idx2 = i;
                 }
             }
@@ -34,48 +34,55 @@ namespace igl
             assert(idx1!=-1);
             assert(idx2!=-1);
             assert(idx1!=-idx2);
+            std::cout << "split test3" << std::endl;
             b->size = idx2 - idx1 + 1;
             a->size = ab->size - b->size + 2;
             a->vertex = Eigen::VectorXi::Zero(a->size);
             b->vertex = Eigen::VectorXi::Zero(b->size); 
             a->vertex.head(idx1+1) = ab->vertex.head(idx1+1);
             a->vertex.tail(a->size-idx1-1) = ab->vertex.tail(a->size-idx1-1);
-            b->vertex = ab->vertex.segment(idx1, idx2+1);
+            b->vertex = ab->vertex.segment(idx1, idx2+1-idx1);
+            std::cout << "split test4" << std::endl;
 
             for (int i = 0; i < ab->size; i++) {
+                std::cout << i << std::endl;
                 if (i < idx1) {
                     a->adjacent_polygon.push_back(ab->adjacent_polygon.at(i));
-                    a->adjacent_index.push_back(i);
-                    ab->adjacent_polygon.at(i)->adjacent_polygon.at(ab->adjacent_index.at(i)) = a;
+                    a->adjacent_index.push_back(ab->adjacent_index.at(i));
+                    if (ab->adjacent_polygon.at(i)) {
+                        ab->adjacent_polygon.at(i)->adjacent_polygon.at(ab->adjacent_index.at(i)) = a;
+                    }
                 }
                 if (i == idx1) {
+                    std::cout << "here" << std::endl;
                     a->adjacent_polygon.push_back(b);
-                    a->adjacent_index.push_back(i);
-                    ab->adjacent_polygon.at(i)->adjacent_polygon.at(ab->adjacent_index.at(i)) = a;
+                    a->adjacent_index.push_back(b->size-1);
                 }
                 if (i >= idx1 && i < idx2) {
+                    std::cout << "or mhere" << std::endl;
                     b->adjacent_polygon.push_back(ab->adjacent_polygon.at(i));
-                    b->adjacent_index.push_back(i);
-                    ab->adjacent_polygon.at(i)->adjacent_polygon.at(ab->adjacent_index.at(i)) = b;
+                    b->adjacent_index.push_back(ab->adjacent_index.at(i));
+                    if (ab->adjacent_polygon.at(i)) {
+                        ab->adjacent_polygon.at(i)->adjacent_polygon.at(ab->adjacent_index.at(i)) = b;
+                    }
                 }
                 if (i == idx2) {
                     b->adjacent_polygon.push_back(a);
-                    b->adjacent_index.push_back(i);
-                    ab->adjacent_polygon.at(i)->adjacent_polygon.at(ab->adjacent_index.at(i)) = b;
+                    b->adjacent_index.push_back(idx1);
                 }
                 if (i >= idx2) {
                     a->adjacent_polygon.push_back(ab->adjacent_polygon.at(i));
-                    a->adjacent_index.push_back(i);
-                    ab->adjacent_polygon.at(i)->adjacent_polygon.at(ab->adjacent_index.at(i)) = a;
+                    a->adjacent_index.push_back(ab->adjacent_index.at(i));
+                    if (ab->adjacent_polygon.at(i)) {
+                        ab->adjacent_polygon.at(i)->adjacent_polygon.at(ab->adjacent_index.at(i)) = a;
+                    }
                 }
             }
+            std::cout << "split test5" << std::endl;
         }
 
         inline void merge (Polygon* a, Polygon* b, Polygon* ab) {
             int idx1, idx2;
-            std::cout << "merge test1" << std::endl;
-            std::cout << a->size << std::endl;
-            std::cout << a->adjacent_polygon.size() << std::endl;
             for (int i = 0; i < a->size; i++) {
                 if (a->adjacent_polygon.at(i) == b) {
                     idx1 = i;
@@ -88,7 +95,6 @@ namespace igl
                     break;
                 }
             }
-            std::cout << "merge test2" << std::endl;
             assert(idx1!=-1);
             assert(idx2!=-1);
             ab->size = a->size + b->size - 2;
@@ -97,25 +103,31 @@ namespace igl
             std::cout << "merge test2.1" << std::endl;
             for (int i = 0; i < ab->size; i++) {
                 if (i < idx1) {
-                    std::cout << "merge test2.2" << std::endl;
                     ab->vertex(i) = a->vertex(i); 
                     ab->adjacent_polygon.push_back(a->adjacent_polygon.at(i));
                     ab->adjacent_index.push_back(a->adjacent_index.at(i));
                 }
-                else if (i <= idx1 + b->size - 1) {
-                    std::cout << "merge test2.3" << std::endl;
+                else if (i < idx1 + b->size - 1) {
+                    std::cout << "case 2" << std::endl;
+                    std::cout << (i - idx1 + idx2 + 1)%b->size << std::endl;
                     ab->vertex(i) = b->vertex((i - idx1 + idx2 + 1)%b->size);
-                    ab->adjacent_polygon.push_back(a->adjacent_polygon.at((i - idx1 + idx2 + 1)%b->size));
-                    ab->adjacent_index.push_back(a->adjacent_index.at((i - idx1 + idx2 + 1)%b->size));
+                    ab->adjacent_polygon.push_back(b->adjacent_polygon.at((i - idx1 + idx2 + 1)%b->size));
+                    ab->adjacent_index.push_back(b->adjacent_index.at((i - idx1 + idx2 + 1)%b->size));
+                    std::cout << "case 2 done" << std::endl;
                 }
                 else {
-                    std::cout << "merge test2.4" << std::endl;
-                    ab->vertex(i) = a->vertex(i + 1 - b->size);
-                    ab->adjacent_polygon.push_back(a->adjacent_polygon.at(i + 1 - b->size));
-                    ab->adjacent_index.push_back(a->adjacent_index.at(i));
+                    std::cout << "case 3" << std::endl;
+                    std::cout << (i - b->size + 2)%a->size << std::endl;
+                    ab->vertex(i) = a->vertex((i - b->size + 2)%a->size);
+                    ab->adjacent_polygon.push_back(a->adjacent_polygon.at((i - b->size + 2)%a->size));
+                    ab->adjacent_index.push_back(a->adjacent_index.at((i - b->size + 2)%a->size));
                 }
-                ab->adjacent_polygon.at(i)->adjacent_index.at(ab->adjacent_index.at(i))= i;
+                if (ab->adjacent_polygon.at(i)) {
+                    ab->adjacent_polygon.at(i)->adjacent_index.at(ab->adjacent_index.at(i))= i;
+                    ab->adjacent_polygon.at(i)->adjacent_polygon.at(ab->adjacent_index.at(i))= ab;
+                }
             }
+            std::cout << ab->vertex << std::endl;
 
             std::cout << "merge test3" << std::endl;
         }
